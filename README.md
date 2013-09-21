2988e5e1671713f6297326a720c974db3495db4d
Hannes Struss <x@hannesstruss.de>
2013-09-21 19:31:08 +0200

    text ui, readme

---
+++ README.md
@@ -0,0 +1,6 @@
+SUPERSCAN
+=========
+
+Makes PDFs from a SANE scanner.
+
+Polite wrapper around `scanimage` and ImageMagick.
--- superscan.py
+++ superscan.py
@@ -1,22 +1,24 @@
+from __future__ import print_function
+
 import argparse
 import subprocess
 import sys
 import os
+import re
 import tempfile
 
 
-def scan_pages(num_pages, filename):
-    if not filename.endswith('.png'):
-        filename = filename + ".png"
+class SuperScanner(object):
+    def __init__(self):
+        self.tmp_folder = tempfile.mkdtemp()
+        self.scanned_files = []
 
-    folder = tempfile.mkdtemp()
-    filename = os.path.join(folder, filename)
+    def get_next_index(self):
+        return len(self.scanned_files) + 1
 
-    all_files = []
+    def scan_page(self):
+        page_name = os.path.join(self.tmp_folder, "scan_%d.png" % self.get_next_index())
 
-    for page in range(num_pages):
-        page_name = filename.replace(".png", "_{:0>4}.png".format(page + 1))
-        print "scanning %s..." % page_name
         scanimage = subprocess.Popen(('scanimage', '--resolution', '300', '--source',
             'Automatic Document Feeder'), stdout=subprocess.PIPE)
         convert = subprocess.Popen(('convert', '-', page_name), stdin=scanimage.stdout,
@@ -24,25 +26,66 @@ def scan_pages(num_pages, filename):
         scanimage.wait()
         convert.wait()
 
-        all_files.append(page_name)
+        self.scanned_files.append(page_name)
 
-    return all_files
+    def make_pdf(self, filename):
+        cmd = ['convert'] + self.scanned_files + [filename]
+        subprocess.call(cmd)
 
 
-def make_pdf(files, outfile):
-    cmd = ['convert'] + files + [outfile]
-    subprocess.call(cmd)
+class ScannerUI(object):
+    INTRX = re.compile(r'^\d+$')
+    MENU = """\
+1. Enter an integer to scan N pages
+2. Enter 'pdf' to generate the PDF"""
+
+    def __init__(self):
+        self.scanner = SuperScanner()
+
+    def start(self):
+        self.running = True
+
+        print("\nGreetings, sir! What project will we be working on today?")
+        self.filename = self.get_filename()
+
+        print("\n%s, very well! And what can I do for you?" % self.filename)
+        self.menu()
+
+    def get_filename(self):
+        name = self.prompt()
+        if not name.endswith('.pdf'):
+            name += '.pdf'
+        return name
+
+    def prompt(self):
+        return raw_input("> ")
+
+    def menu(self):
+        print(self.MENU)
+        while self.running:
+            cmd = self.prompt()
+            if self.INTRX.match(cmd):
+                print()
+                for n in range(int(cmd)):
+                    print("Page %d in the works, sir!" %
+                        self.scanner.get_next_index())
+                    self.scanner.scan_page()
+                print("\nAlright! What now?")
+                print(self.MENU)
+            elif cmd == "kthx":
+                self.running = False
+                print("Have a nice day, sir!\n\n")
+            elif cmd == 'pdf':
+                print("\nRight away, sir, just one moment!")
+                self.scanner.make_pdf(self.filename)
+                print("There you go, sir, fresh and warm!\n")
+                self.running = False
+            else:
+                print("\nI'm sorry, what?")
 
 
 def main():
-    parser = argparse.ArgumentParser(description='''Don't-forget-the-values-wrapper around scanimage''')
-    parser.add_argument('--pages', type=int, help='Scan that many pages')
-    parser.add_argument('--filename', type=str, help='Use that as the filename')
-    args = parser.parse_args()
-    
-    pages = scan_pages(args.pages, args.filename)
-    make_pdf(pages, args.filename + ".pdf")
-    # map(os.remove, pages)
+    ScannerUI().start()
 
 
 if __name__ == '__main__':
